f252e1588c20b196acb12c6d1c0af617d1f11755
Paz Constanza Gutiérrez Césped <paz.constanzagc@gmail.com>
2024-06-03 18:36:19 -0400

    Pasa todos los test

--- src/analyzer.js
+++ src/analyzer.js
@@ -19,25 +19,25 @@ const analyzer = {
   
   
   getCharacterCountExcludingSpaces: (text) => {
-    return text.replace(/\s+/g, "").length;
+    return text.replace(/[\s.,]+/g, "").length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
   
 
   getNumberCount: (text) => {
-    return (text.match(/\d+/g) || []).length
+    return (text.match(/\b\d+(\.\d+)?\b/g) || []).length
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   
   getNumberSum: (text) => {
-    let palabras=text.split(' ')
+    const palabras = text.match(/\b\d+(\.\d+)?\b/g) || [];
     
     let suma=0
     palabras.forEach(palabra => {
       if(!isNaN(palabra) && palabra.trim() !== "") {
-        suma += parseInt(palabra); 
-        console.log(suma)
-    }
+        suma += parseFloat(palabra); 
+        
+      }
       
     });
     return suma
@@ -47,12 +47,12 @@ const analyzer = {
 
 
   getAverageWordLength: (text) => { 
-    let totalpalabras=text.split(" ");
-    let totalcaracteres=text.replace(/\s+/g, "").length
-    let Promedio=totalcaracteres/totalpalabras.length
-    return Promedio;
+    const totalpalabras=text.split(" ");
+    const totalcaracteres=text.replace(/\s+/g, "").length
+    const Promedio=totalcaracteres/totalpalabras.length
+    return parseFloat (Promedio.toFixed(2));
   //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-},
+  },
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -3,39 +3,34 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 
-let texto=document.querySelector('textarea[name="user-input"]');
-let li=document.querySelectorAll("li.contadores");
-
+const texto=document.querySelector('textarea[name="user-input"]');
+const li=document.querySelectorAll("li.contadores");
 
 
 
 texto.addEventListener("input", function(){
     
-    li[0].textContent = "Caracteres: " + analyzer.getCharacterCount(texto.value)
-    li[1].textContent = "Caracteres sin espacio: " + analyzer.getCharacterCountExcludingSpaces(texto.value)
-    li[2].textContent = "Palabras: " + analyzer.getWordCount(texto.value)
-    li[3].textContent = "Números: " + analyzer.getNumberCount(texto.value)
-    li[4].textContent = "Suma Números : " + analyzer.getNumberSum(texto.value)
-    li[5].textContent = "Promedio longitud : " + analyzer.getAverageWordLength(texto.value)
-
-    
+  li[0].textContent = "Caracteres: " + analyzer.getCharacterCount(texto.value)
+  li[1].textContent = "Caracteres sin espacio: " + analyzer.getCharacterCountExcludingSpaces(texto.value)
+  li[2].textContent = "Palabras: " + analyzer.getWordCount(texto.value)
+  li[3].textContent = "Números: " + analyzer.getNumberCount(texto.value)
+  li[4].textContent = "Suma Números : " + analyzer.getNumberSum(texto.value)
+  li[5].textContent = "Promedio longitud : " + analyzer.getAverageWordLength(texto.value)
     
 })
 
-
-
-
-let button=document.getElementById("reset-button")
+                  
+const button=document.getElementById("reset-button")
 button.addEventListener("click", function(){
-    texto.value="";
+  texto.value="";
     
 
-    li[0].textContent = "Caracteres: 0" 
-    li[1].textContent = "Caracteres sin espacio: 0" 
-    li[2].textContent = "Palabras: 0" 
-    li[3].textContent = "Números: 0" 
-    li[4].textContent= "Suma Números: 0"
-    li[5].textContent= "Promedio longitud: 0"
+  li[0].textContent = "Caracteres: 0" 
+  li[1].textContent = "Caracteres sin espacio: 0" 
+  li[2].textContent = "Palabras: 0" 
+  li[3].textContent = "Números: 0" 
+  li[4].textContent= "Suma Números: 0"
+  li[5].textContent= "Promedio longitud: 0"
    
 })
 
