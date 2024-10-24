748c7db49b7303f37d1410c35651c8761d6132ec
Paz Constanza Gutiérrez Césped <paz.constanzagc@gmail.com>
2024-10-24 15:34:44 -0300

    modificaciones de estilo

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const texto = document.querySelector('textarea[name="user-input"]');
+const li = document.querySelectorAll("li.contadores");
 
+texto.addEventListener("input", function () {
 
-const texto=document.querySelector('textarea[name="user-input"]');
-const li=document.querySelectorAll("li.contadores");
-
-
-
-texto.addEventListener("input", function(){
-    
   li[0].textContent = "Caracteres: " + analyzer.getCharacterCount(texto.value)
   li[1].textContent = "Caracteres sin espacio: " + analyzer.getCharacterCountExcludingSpaces(texto.value)
   li[2].textContent = "Palabras: " + analyzer.getWordCount(texto.value)
   li[3].textContent = "Números: " + analyzer.getNumberCount(texto.value)
   li[4].textContent = "Suma Números : " + analyzer.getNumberSum(texto.value)
   li[5].textContent = "Promedio longitud : " + analyzer.getAverageWordLength(texto.value)
-    
-})
 
-                  
-const button=document.getElementById("reset-button")
-button.addEventListener("click", function(){
-  texto.value="";
-    
+});
 
-  li[0].textContent = "Caracteres: 0" 
-  li[1].textContent = "Caracteres sin espacio: 0" 
-  li[2].textContent = "Palabras: 0" 
-  li[3].textContent = "Números: 0" 
-  li[4].textContent= "Suma Números: 0"
-  li[5].textContent= "Promedio longitud: 0"
-   
-})
+
+const button = document.getElementById("reset-button")
+button.addEventListener("click", function () {
+  texto.value = "";
+
+  li[0].textContent = "Caracteres: 0"
+  li[1].textContent = "Caracteres sin espacio: 0"
+  li[2].textContent = "Palabras: 0"
+  li[3].textContent = "Números: 0"
+  li[4].textContent = "Suma Números: 0"
+  li[5].textContent = "Promedio longitud: 0"
+
+});
 
